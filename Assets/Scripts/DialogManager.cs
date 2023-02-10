a63d96fc0bb76025d469c5e14bfc607ead3dd5e9
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2023-02-10 15:01:18 -0300

    write dialog

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,41 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour {
 
-    private string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis erat nulla. Mauris eget accumsan turpis. Ut ac dignissim lectus. Mauris pretium lobortis placerat. Nunc sagittis dignissim erat sed faucibus. Pellentesque orci dui, interdum in erat quis, ullamcorper molestie nulla. Praesent commodo elit tortor, quis auctor eros rhoncus ac. Praesent ut mollis est, ac bibendum quam. Quisque facilisis mollis lobortis. Pellentesque non mauris dolor. Proin aliquet aliquam risus ut ultrices. Quisque et convallis massa. Curabitur vitae pulvinar justo. Nullam hendrerit mauris sed tellus vehicula, id lobortis lectus ultrices. Integer ac augue auctor, volutpat purus nec, pretium justo.\r\n\r\n";
     public Text dialog_text;
-
-
+    
     public float writeSpeed;
-    private protected float timer = 0;
+    private protected float writeTimer = 0;
+
+    private string text = "Lorem ipsum dolor sit amet, /a consectetur adipiscing elit. Proin quis erat nulla. Mauris eget accumsan turpis. Ut ac dignissim lectus. Mauris pretium lobortis placerat. Nunc sagittis dignissim erat sed faucibus. Pellentesque orci dui, interdum in erat quis, ullamcorper molestie nulla. Praesent commodo elit tortor, quis auctor eros rhoncus ac.";
 
     private int currentChar = 0;
 
-    void Start() {
+    private void Update() {
+        if (currentChar < text.Length) {
+            WriteText();
+        } else {
 
+        }
     }
 
-    void Update() {
-        if(currentChar <= text.Length) {            
-            if(timer > writeSpeed) {
-                
+    private void WriteText() {
+        if(writeTimer >= writeSpeed) {
+            CheckAlteration(text[currentChar]);
+            dialog_text.text += text[currentChar];
+            
+            currentChar++;
+            writeTimer = 0;
+        } else {
+            writeTimer += Time.deltaTime;
+        }
+
+        void CheckAlteration(char character) {
+            if(character == '/') {
+                currentChar++;
                 switch(text[currentChar]) {
-                    case '\a': Debug.Log("a");
-                        dialog_text.text += text[currentChar];
+                    case 'a':
+                        Debug.Log("a command");
                         break;
 
+                    case 'b':
+                        Debug.Log("b command");
+                        break;
+                    
                     default:
-                        dialog_text.text += text[currentChar];
+                        Debug.LogError("/" + text[currentChar] + " is not a valid command");         
                         break;
                 }
-
-                currentChar++;
-                timer = 0;
-            } else {
-                timer += Time.deltaTime;
+                currentChar += 2;
             }
         }
     }
